34d2d1b39fb95b2e539958ce35662e107e6f7c1d
Arulkumar046 <mersalarul53@gmail.com>
2024-07-24 09:26:12 +0530

    Add files via upload

---
+++ Linked list/Circular Linked List.java
@@ -0,0 +1,69 @@
+import java.util.*;
+class CLL
+{
+    Node head;
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int val)
+        {
+            data=val;
+            next=null;
+        }
+    }
+CLL()
+{
+    head=null;
+}
+public void insertpos(int pos,int val2)
+    {
+     Node newnode = new Node(val2);
+     Node temp = head;
+     for(int i=1 ;i<pos ;i++)
+     {
+      temp=temp.next;
+     }
+     newnode.next=temp.next;
+     temp.next = newnode;   
+    }
+public void insertend(int val) {
+            Node newnode=new Node(val);
+            if( head==null) {
+                newnode.next = newnode;
+                head = newnode;
+                
+            }else {
+                newnode.next = head.next;
+                head.next = newnode;
+                head = newnode;
+            }
+        }
+    public void display()
+    {
+        Node temp=head.next;
+        do{
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        while(temp!=head.next);
+    }
+    public static void main(String[]args)
+    {
+        Scanner obj=new Scanner(System.in);
+        CLL list=new CLL();
+        int n=obj.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int val=obj.nextInt();
+            list.insertend(val);
+        }
+        list.display();
+        System.out.print("\nEnter the element at mid position\n");
+        int pos=obj.nextInt();
+        int val2=obj.nextInt();
+        list.insertpos(pos,val2);
+        list.display();
+        obj.close();
+    }
+}
