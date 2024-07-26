5889a26471f5524c1b0d3ea5035100d6b02b69cf
Arulkumar G <mersalarul53@gmail.com>
2024-07-26 10:33:22 +0530

    Add files via upload

---
+++ Data Structures/DLL.java
@@ -0,0 +1,92 @@
+class Node 
+{
+     int data;
+     Node prev;
+     Node next;
+     Node(int data) 
+    {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class DLL {
+     Node head;
+     Node tail;
+    DLL() {
+        this.head = null;
+        this.tail = null;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public void insertFirst(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+    public void insertLast(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+    public void deleteFirst() {
+        if (!isEmpty()) {
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head = head.next;
+                head.prev = null;
+            }
+        }
+    }
+    public void deleteLast() {
+        if (!isEmpty()) {
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                tail = tail.prev;
+                tail.next = null;
+            }
+        }
+    }
+    void display() {
+        Node current = head;
+        System.out.print("Doubly Linked List: ");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+public class DLList
+{
+    public static void main(String[] args) {
+        DLL list = new DLL();
+
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertLast(3);
+
+        list.display();
+        list.deleteFirst();
+        list.deleteLast();
+
+        list.display();
+    }
+}
