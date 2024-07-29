217ca4f5229d5c9ea8ac8758525b37aa3476cab1
Arulkumar G <mersalarul53@gmail.com>
2024-07-28 22:40:13 -0700

    Add files via upload

---
+++ Data Structures/InfixToPostfix.java
@@ -0,0 +1,61 @@
+import java.util.Stack;
+import java.util.*;
+public class InfixToPostfix {
+
+    private static int precedence(char operator) {
+        switch (operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    public static String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+            
+            if (Character.isLetterOrDigit(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+        
+        return postfix.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String infix = scanner.nextLine();
+        
+        String postfix = infixToPostfix(infix);
+        System.out.println(postfix);
+        
+        scanner.close();
+    }
+}
