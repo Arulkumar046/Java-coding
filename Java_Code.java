1144d72519460d4a5b499912034d63db97c18aea
Arulkumar G <mersalarul53@gmail.com>
2024-07-30 01:07:57 -0700

    Update and rename Java_code.java to Java_Code.java

--- Java_Code.java
+++ Java_Code.java
@@ -1,6 +1,6 @@
 //1.Scan a string and print all the characters until it meets lowercase character.
 import java.util.*;
-class Java_code{
+class Java_Code{
   public static void main(String[] args){
     Scanner n=new Scanner(System.in);
     String str=n.nextLine();
