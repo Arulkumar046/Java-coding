28237597498d0ab0ab3f477dd692db293c170710
Arulkumar G <mersalarul53@gmail.com>
2024-08-05 05:07:14 -0700

    Add files via upload

---
+++ Simple programs/Left_Rotation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Left_Rotation
+{
+  public static void main(String[]args)
+  {
+    Scanner obj=new Scanner(System.in);
+    int n=obj.nextInt();
+    int[] a=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      a[i]=obj.nextInt();
+    }
+    int r=obj.nextInt();
+    int[] res=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      res[i]=a[(i+r)%n];
+    }
+    for(int i=0;i<n;i++)
+    {
+      System.out.printf("%d ",res[i]);
+    }
+    
+  }
+}
