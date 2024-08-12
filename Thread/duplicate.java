249cd7c5c5b174e5bec85594331f4797a7849057
Arulkumar G <mersalarul53@gmail.com>
2024-08-12 09:50:19 +0530

    Update duplicate.java

--- Thread/duplicate.java
+++ Thread/duplicate.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class duplicate {
+public class Duplicate {
     public static void main(String[] args){
         Scanner ob=new Scanner(System.in);
         int n=ob.nextInt();
