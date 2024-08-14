c2c1bbc6326af315eede0b99d49a957973eae6bc
Arulkumar G <mersalarul53@gmail.com>
2024-08-13 21:42:11 -0700

    Add files via upload

---
+++ Simple programs/snake_pattern.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class snake_pattern
+{
+  public static void main(String[]args)
+  {
+    Scanner obj=new Scanner(System.in);
+    int row=obj.nextInt();
+    int col=obj.nextInt();
+    int[][]arr=new int[row][col];
+    for(int i=0;i<row;i++)
+    {
+      for(int j=0;j<col;j++)
+      {
+        arr[i][j]=obj.nextInt();
+    }
+  }
+  for(int i=0;i<row;i++)
+  {
+    if(i%2==0)
+    {
+      for(int j=0;j<col;j++)
+      {
+        System.out.print(arr[i][j]+" ");
+    }
+    System.out.println();
+  }else
+  {
+    for(int j=col-1;j>=0;j--)
+    {
+      System.out.print(arr[i][j]+" ");
+    }
+    System.out.println();
+  }
+}
+}
+}
