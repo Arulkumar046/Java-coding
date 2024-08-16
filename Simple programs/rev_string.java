bd0e2b3346f596d797565fe20a991a9177714797
Arulkumar G <mersalarul53@gmail.com>
2024-08-16 04:51:53 -0700

    Add files via upload

---
+++ Simple programs/rev_string.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class rev_string
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    
+    String str = sc.nextLine();
+    
+    String[] arr = str.split(" ");
+    
+    StringBuilder string = new StringBuilder();
+    
+    for(int i = 0;i < arr.length;i++)
+    {
+      String str2 = rev(arr[i]);
+      string.append(str2).append(" ");
+      
+    }
+    System.out.print(string.toString().trim());
+  }
+  public static  String rev(String name)
+  {
+    String str3 = name;
+    String empty = "";
+    
+    for(int i =str3.length() - 1;i >=0;i--)
+    {
+      empty = empty + str3.charAt(i);
+    }
+    return empty;
+  }
+}
