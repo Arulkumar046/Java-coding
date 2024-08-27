58e5eafc024dd098bc008828f41017ae46718a87
Arulkumar G <mersalarul53@gmail.com>
2024-08-27 17:07:28 +0530

    Update Java_excercise.java

--- Java_excercise.java
+++ Java_excercise.java
@@ -1,6 +1,6 @@
 //1.Scan a string and print all the characters until it meets lowercase character.
 import java.util.*;
-class Javaexcercise{
+class Java_excercise{
   public static void main(String[] args){
     Scanner n=new Scanner(System.in);
     String str=n.nextLine();
