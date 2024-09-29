0c1d95d731f292509b22b5829e504b1c907e3be3
Arulkumar G <mersalarul53@gmail.com>
2024-09-29 18:39:17 +0530

    Update Min_Max.java

--- Simple programs/Min_Max.java
+++ Simple programs/Min_Max.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
-class Node {
+class Node
+    {
     int data;
     Node next;
 
-    Node(int d) {
+    Node(int d)
+        {
         data = d;
         next = null;
-    }
+        }
 }
 
 class scl
