89be97e79cd25105e517ec8b8950d06c92a7da73
Arulkumar G <mersalarul53@gmail.com>
2024-10-06 17:57:40 +0530

    Update Reverse_Num.java

--- Simple programs/Reverse_Num.java
+++ Simple programs/Reverse_Num.java
@@ -1,18 +1,19 @@
 import java.util.*;
 public class Reverse_Num  
-{  
-      public static void main(String[] args){
+ {  
+      public static void main(String[] args)
+      {
       Scanner obj=new Scanner(System.in);
-    {  
+      {  
       System.out.print("Enter a number : ");
       int number =obj.nextInt();int reverse = 0; 
       while(number != 0)   
-    {  
+      {  
       int remainder = number % 10;  
       reverse = reverse * 10 + remainder;  
       number = number/10;  
-    }  
+      }  
       System.out.println("The reverse of the given number is: " + reverse);  
-    }  
-    }   
-}
+      }  
+      }   
+ }
