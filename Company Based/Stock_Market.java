e8fb086349422f5d5337e6ce2cd54094ba52def0
Arulkumar G <mersalarul53@gmail.com>
2024-10-09 01:35:10 -0700

    Add files via upload

---
+++ Company Based/Stock_Market.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Stock_Market {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.println("Enter the Stock Product:");
+        int Product = obj.nextInt();
+        
+        int count = 0;
+        int[] arr = new int[Product];
+        
+        System.out.println("Enter the Stock prices:");
+        for (int i = 0; i < Product; i++) {
+            arr[i] = obj.nextInt();
+        }
+        
+        for (int i = 0; i < Product; i++) {
+            if (arr[i] < 0) {
+                count += 1;
+            }
+        }
+        
+        System.out.print(count);
+    }
+}
