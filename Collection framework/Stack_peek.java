fbbe5ee705e9f5e5c95a0a7ebe322f893943d8a3
Arulkumar G <mersalarul53@gmail.com>
2024-10-15 08:17:29 -0700

    Update Stack_peek.java

--- Collection framework/Stack_peek.java
+++ Collection framework/Stack_peek.java
@@ -4,7 +4,7 @@ class Stack_peek
 {
     public static void main(String[] args) 
     {
-        Stack<Integer> numbers= new Stack<Integer>();
+        Stack<Integer> numbers= new Stack<>();
         numbers.push(20);
         numbers.push(10);
         numbers.push(50);
